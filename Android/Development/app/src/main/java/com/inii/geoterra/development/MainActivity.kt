980324923b6b0ac46ab0e09dcdf167564654b999
Svndda <aaroncz032@gmail.com>
2024-04-09 21:30:40 -0600

    Add Initial Android Setup

---
+++ Android/Development/app/src/main/java/com/inii/geoterra/development/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.inii.geoterra.development
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnPulsame = findViewById<Button>(R.id.btnPulsame)
+        btnPulsame.setOnClickListener {navigateToPulse()}
+
+    }
+
+    fun navigateToPulse() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
