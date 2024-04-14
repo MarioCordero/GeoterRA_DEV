45f05c214f4fb891a4a86076541ecd5f4125c703
chris4fun <christopheracostam14@gmail.com>
2024-04-13 22:59:26 -0600

    Conexion con la base de datos

--- website/includes/dbhandler.inc.php
+++ website/includes/dbhandler.inc.php
@@ -1,16 +1,16 @@
 <?php
-// Database connection parameters
+
 $host = 'localhost';
 $dbname = 'users';
-$user = 'ch4f';
-$pass = 'C1h8r0i4s';
+$user = 'root';
+$pass = '';
 
-echo "users connecetion";
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=#$dbname, $user, $pass");
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $user, $pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  echo "Succesfully connected<br>";
 } catch (PDOException $e) {
   die("Connection failed: " . $e->getMessage());
 }
-
 ?>
