c53a3764ee04b9a0e68fea31dc991992e49b4cbc
chris4fun <christopheracostam14@gmail.com>
2024-05-19 18:34:27 -0600

    Creacion de un cookie para enviar puntos entre paginas

--- website/assets/js/interactive-map.js
+++ website/assets/js/interactive-map.js
@@ -1,13 +1,38 @@
 function onMarkerClick() {
   let latlng = this.getLatLng(); // 'this' refers to the marker clicked
   let pointObt = this.point;
-  let content = "<div id= 'cont'>Lugar: " + pointObt.id + "<br>Longitude: " + latlng.lng.toFixed(4) + "<br>Latitude: " + latlng.lat.toFixed(4) + "</div>"
+
+  let content = `<div id='cont'>
+    <p>
+        Lugar: ${pointObt.id}<br>
+        Longitude: ${latlng.lng.toFixed(4)}<br>
+        Latitude: ${latlng.lat.toFixed(4)}<br>
+        <a id='showPoint' href='./show_point.html'>Show more</a>
+    </p>
+  </div>`;
+
   // Create a popup with the coordinates
    L.popup()
   .setLatLng(latlng)
   .setContent(content)
+  .bindPopup('height= 100px')
   .openOn(map);
+
+  console.log("AT some point")
+  const toShow = document.getElementById('showPoint');
+
+  // Crea un evento cuando se clickea el link de mostrar mas
+  toShow.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log("Valor apunto de enviar " + pointObt.id);
+
+    let stringPoint = JSON.stringify(pointObt);
+    document.cookie = "pointObject=" + stringPoint + ";path=/;";
+    window.location.href = "./show_point.html";
+  });
+
 }
+
 
 function fetchData() {
   return new Promise ((resolve, reject) => {
@@ -17,7 +42,6 @@ function fetchData() {
     xhr.open("POST", "assets/includes/map_data.inc.php", true);
     xhr.send(region);
     xhr.onreadystatechange = function () {
-
       if (xhr.readyState == 4 && xhr.status == 200) {
         let splittedResponse = xhr.responseText.split("}");
         for (let i = 0; i < splittedResponse.length - 1; i++) {
@@ -48,7 +72,6 @@ function createMarkers(obtainedPoints) {
     markers[i].on('click', onMarkerClick);
   }
   // Here are the markers created
-
 
   console.log(obtainedPoints[0].id);
   console.log(obtainedPoints[0].coord_x)
