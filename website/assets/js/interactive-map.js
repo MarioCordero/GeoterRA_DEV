eece896dd44b042dc398ae0b29caf4b88be30c6d
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-05-24 12:14:03 -0600

    - Modificado CSS para map.html
    - Modificado map.html para una estructura distinta
    
    - Modificado interactive-map.js para diversas capas

--- website/assets/js/interactive-map.js
+++ website/assets/js/interactive-map.js
@@ -1,19 +1,32 @@
+// Define the map and set its initial view
+let map = L.map('map').setView([9.9358333333333, -84.050555555556], 17);
+
+// Define the base layer using OpenStreetMap tiles
+let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  icon: false
+}).addTo(map);
+
+// Define another base layer using Mapbox Satellite
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=YOUR_MAPBOX_ACCESS_TOKEN', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  tileSize: 512,
+  zoomOffset: -1
+});
+
+// Function to convert coordinates from projected to geographic
 function convertCoordinates(easting, northing) {
-  // Define the projections
   var projFrom = "+proj=tmerc +lat_0=0 +lon_0=-84 +k=0.9996 +x_0=500000 +y_0=0 +ellps=GRS80 +datum=WGS84 +units=m +no_defs";
   var projTo = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-
-  // Perform the transformation
   var [longitude, latitude] = proj4(projFrom, projTo, [easting, northing]);
-
-  console.log(latitude, longitude);
-  return [latitude, longitude]
+  return [latitude, longitude];
 }
 
+// Function to handle marker click events
 function onMarkerClick() {
-  let latlng = this.getLatLng(); // 'this' refers to the marker clicked
+  let latlng = this.getLatLng();
   let pointObt = this.point;
-
   let content = `<div id='cont'>
     <p>
         Lugar: ${pointObt.id}<br>
@@ -23,31 +36,25 @@ function onMarkerClick() {
     </p>
   </div>`;
 
-  // Create a popup with the coordinates
-   L.popup()
-  .setLatLng(latlng)
-  .setContent(content)
-  .bindPopup('height= 100px')
-  .openOn(map);
+  L.popup()
+    .setLatLng(latlng)
+    .setContent(content)
+    .bindPopup('height= 100px')
+    .openOn(map);
 
-  console.log("AT some point")
   const toShow = document.getElementById('showPoint');
-
-  // Crea un evento cuando se clickea el link de mostrar mas
   toShow.addEventListener('click', function(event) {
     event.preventDefault();
-    console.log("Valor apunto de enviar " + pointObt.id);
-
     let stringPoint = JSON.stringify(pointObt);
     document.cookie = "pointObject=" + stringPoint + ";path=/;";
     window.location.href = "./show_point.html";
   });
-
 }
 
+// Function to fetch data from the server
 function fetchData() {
-  return new Promise ((resolve, reject) => {
-    let region = "Guanacaste"
+  return new Promise((resolve, reject) => {
+    let region = "Guanacaste";
     let obtainedPoints = [];
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "assets/includes/map_data.inc.php", true);
@@ -58,38 +65,41 @@ function fetchData() {
         for (let i = 0; i < splittedResponse.length - 1; i++) {
           splittedResponse[i] += '}';
         }
-        console.log(splittedResponse);
         for (let i = 0; i < splittedResponse.length - 1; i++) {
           obtainedPoints[i] = JSON.parse(splittedResponse[i]);
         }
         resolve(obtainedPoints);
-      } 
-    }
+      }
+    };
   });
 }
 
+// Function to create markers on the map
 function createMarkers(obtainedPoints) {
-  // markers[0] = L.marker([pointsObtained[0].coord_x, pointsObtained[0].coord_y]).addTo(map)
-  let markers = []
-
+  let markers = [];
   for (let i = 0; i < obtainedPoints.length; i++) {
-    pointXY = convertCoordinates(obtainedPoints[i].coord_x, obtainedPoints[i].coord_y)
-    markers[i] = L.marker([pointXY[0],pointXY[1]]).addTo(map); 
+    let pointXY = convertCoordinates(obtainedPoints[i].coord_x, obtainedPoints[i].coord_y);
+    markers[i] = L.marker([pointXY[0], pointXY[1]]).addTo(map);
     markers[i].point = obtainedPoints[i];
     markers[i].on('click', onMarkerClick);
-    console.log(i);
   }
-
-  console.log(obtainedPoints);
 }
 
-// No se ordenar .JS Mario ayuda lo de arriba son solo funciones
-
-let map = L.map('map').setView([9.9358333333333, -84.050555555556], 17);
-
-let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  icon: false
-}).addTo(map);
-
+// Fetch data and create markers on the map
 fetchData().then(value => createMarkers(value));
+
+// Adding Layer Control
+let baseLayers = {
+  "OpenStreetMap": osm,
+  "Limón": satellite,
+  "Guancaste": satellite,
+  "San Jose": satellite,
+  "Alajuela": satellite
+};
+
+let overlayLayers = {
+  // Add any overlay layers you want here, for example:
+  // "Layer Name": someLeafletLayer
+};
+
+L.control.layers(baseLayers, overlayLayers).addTo(map);
