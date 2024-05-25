7bad36f86a8312c71164b2b87463fa6c65a9d614
chris4fun <christopheracostam14@gmail.com>
2024-05-24 18:23:46 -0600

    Agregado de detalles visuales diagrama de Piper

--- website/assets/js/show-point.js
+++ website/assets/js/show-point.js
@@ -8,9 +8,8 @@ const myElement = document.getElementById('resultChanged');
 myElement.textContent = pointObject.id + " " + pointObject.coord_x + " " + pointObject.coord_y;
 
 // Grafico
-
-const margin = {top: 20, right: 20, bottom: 20, left: 20};
-const width = 800;
+const margin = ({ top: 0, right: 0, bottom: 35, left: 0 })
+const width = 600;
 const height = 600;
 
 let svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
@@ -19,8 +18,8 @@ let g = svg
 .append("g")
 .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-let xScale = d3.scaleLinear().domain([0, 2.4]).range([0, width])
-let yScale = d3.scaleLinear().domain([0, 2.4]).range([height, 0]);
+let x = d3.scaleLinear().domain([0, 2.4]).range([0, width])
+let y = d3.scaleLinear().domain([0, 2.4]).range([height, 0]);
 
 xAxis = (g) =>
   g.attr("transform", `translate(0,${height})`).call(d3.axisBottom(x))
@@ -143,10 +142,10 @@ for (let i in lines) {
     .style("stroke-width", 0.5)
     .attr("stroke", "gray")
     .style("stroke-dasharray", "5,1")
-    .attr("x1", xScale(lines[i].x0))
-    .attr("y1", yScale(lines[i].y0))
-    .attr("x2", xScale(lines[i].x1))
-    .attr("y2", yScale(lines[i].y1));
+    .attr("x1", x(lines[i].x0))
+    .attr("y1", y(lines[i].y0))
+    .attr("x2", x(lines[i].x1))
+    .attr("y2", y(lines[i].y1));
 }
 // Triangulo Exterior
 
@@ -219,40 +218,98 @@ Triangle = [
 
 for (let i in Triangle) {
   g.append("line")
-    .style("stroke-width", 0.5)
-    .attr("stroke", "gray")
+    .style("stroke-width", 2)
+    .attr("stroke", "black")
     .style("stroke-dasharray", "5,1")
-    .attr("x1", xScale(Triangle[i].x0))
-    .attr("y1", yScale(Triangle[i].y0))
-    .attr("x2", xScale(Triangle[i].x1))
-    .attr("y2", yScale(Triangle[i].y1));
+    .attr("x1", x(Triangle[i].x0))
+    .attr("y1", y(Triangle[i].y0))
+    .attr("x2", x(Triangle[i].x1))
+    .attr("y2", y(Triangle[i].y1));
 }
+
+// Etiquetas de los valores
+
+TickLabels = [
+   {x: 0.385, y: 0.17, text: "80", rot: "0", FontSize: 8}
+  ,{x: 0.5850000000000001, y: 0.17, text: "60", rot: "0", FontSize: 8}
+  ,{x: 0.785, y: 0.17, text: "40", rot: "0", FontSize: 8}
+  ,{x: 0.985, y: 0.17, text: "20", rot: "0", FontSize: 8}
+  ,{x: 0.58, y: 0.8828203230275509, text: "80", rot: "-60", FontSize: 8}
+  ,{x: 0.4799999999999999, y: 0.7096152422706632, text: "60", rot: "-60", FontSize: 8}
+  ,{x: 0.37999999999999995, y: 0.5364101615137755, text: "40", rot: "-60", FontSize: 8}
+  ,{x: 0.2799999999999999, y: 0.3632050807568877, text: "20", rot: "-60", FontSize: 8}
+  ,{x: 0.8049999999999999, y: 0.9128203230275509, text: "20", rot: "60", FontSize: 8}
+  ,{x: 0.9049999999999999, y: 0.7396152422706632, text: "40", rot: "60", FontSize: 8}
+  ,{x: 1.005, y: 0.5664101615137755, text: "60", rot: "60", FontSize: 8}
+  ,{x: 1.105, y: 0.3932050807568877, text: "80", rot: "60", FontSize: 8}
+  ,{x: 1.585, y: 0.17, text: "80", rot: "0", FontSize: 8}
+  ,{x: 1.785, y: 0.17, text: "60", rot: "0", FontSize: 8}
+  ,{x: 1.985, y: 0.17, text: "40", rot: "0", FontSize: 8}
+  ,{x: 2.185, y: 0.17, text: "20", rot: "0", FontSize: 8}
+  ,{x: 1.7799999999999998, y: 0.8828203230275509, text: "80", rot: "-60", FontSize: 8}
+  ,{x: 1.68, y: 0.7096152422706632, text: "60", rot: "-60", FontSize: 8}
+  ,{x: 1.5799999999999998, y: 0.5364101615137755, text: "40", rot: "-60", FontSize: 8}
+  ,{x: 1.48, y: 0.3632050807568877, text: "20", rot: "-60", FontSize: 8}
+  ,{x: 2.005, y: 0.9128203230275509, text: "20", rot: "60", FontSize: 8}
+  ,{x: 2.105, y: 0.7396152422706632, text: "40", rot: "60", FontSize: 8}
+  ,{x: 2.2049999999999996, y: 0.5664101615137755, text: "60", rot: "60", FontSize: 8}
+  ,{x: 2.3049999999999997, y: 0.3932050807568877, text: "80", rot: "60", FontSize: 8}
+  ,{x: 0.882, y: 1.4074355652982142, text: "20", rot: "-60", FontSize: 8}
+  ,{x: 0.982, y: 1.580640646055102, text: "40", rot: "-60", FontSize: 8}
+  ,{x: 1.082, y: 1.7538457268119896, text: "60", rot: "-60", FontSize: 8}
+  ,{x: 1.1820000000000002, y: 1.9270508075688773, text: "80", rot: "-60", FontSize: 8}
+  ,{x: 1.7049999999999998, y: 1.4324355652982141, text: "20", rot: "60", FontSize: 8}
+  ,{x: 1.605, y: 1.6056406460551018, text: "40", rot: "60", FontSize: 8}
+  ,{x: 1.505, y: 1.7788457268119895, text: "60", rot: "60", FontSize: 8}
+  ,{x: 1.4049999999999998, y: 1.9520508075688772, text: "80", rot: "60", FontSize: 8}
+]
+
+TextPos = [
+  {x: 1.6099999999999999, y: 1.6922431864335457, text: "Calcium + Magnesium", rot: "60", FontSize: 18}
+  ,{x: 0.9949999999999999, y: 1.6922431864335457, text: "Sulfate + Chloride", rot: "-60", FontSize: 18}
+  ,{x: 1, y: 0.6830127018922194, text: "Sodium + Potassium", rot: "60", FontSize: 18}
+  ,{x: 1.5999999999999999, y: 0.6830127018922194, text: "Carbonate + Bicarbonate", rot: "-60", FontSize: 18}
+  ,{x: 0.41000000000000003, y: 0.6730127018922194, text: "Magnesium", rot: "-60", FontSize: 18}
+  ,{x: 2.19, y: 0.6730127018922194, text: "Sulfate", rot: "60", FontSize: 18}
+  ,{x: 0.7, y: 0.1, text: "Calcium", rot: "0", FontSize: 18}
+  ,{x: 1.9, y: 0.1, text: "Chloride", rot: "0", FontSize: 18}
+]
+
+svg.selectAll("text.rotation")
+    .data(TextPos.concat(TickLabels))
+    .enter()
+    .append("text")
+    .text((d) => d.text)
+    .classed("rotation", true)
+    .attr("fill", "black")
+    .style("font-size", (d) => d.FontSize)
+    .attr("transform", (d, i) => {
+      return (
+        "translate( " + x(d.x) + " , " + y(d.y) + ")," + "rotate(" + d.rot + ")"
+      );
+    })
+    .attr("x", 0)
+    .style("text-anchor", "middle")
+    .attr("y", 0);
 
 console.log(lines)
 
-
 const existingSvg = document.getElementById("piperDiagram");
 
-// Set the innerHTML of the existing SVG element to the SVG content
+// dataEq = data.map((x) => {
+//   x.Potassium /= 39.1;
+//   x.Sodium /= 22.9;
+//   x.Bicarbonate /= 61.02;
+//   x.Carbonate /= 30;
+//   x.Calcium /= 20.04;
+//   x.Magnesium /= 12.15;
+//   x.Chloride /= 35.45;
+//   x.Sulfate /= 48.03;
+//   return x;
+// })
+
 d3.select(existingSvg)
   .append(() => svg.node());
-
-// TriangleFillLines.forEach(line => {
-//   g.append("line")
-//     .attr("x1", xScale(line.x0))
-//     .attr("y1", yScale(line.y0))
-//     .attr("x2", xScale(line.x1))
-//     .attr("y2", yScale(line.y1))
-//     .attr("stroke", "lightgrey")
-//     .attr("stroke-width", 1);
-// });
-
-// Example data points (replace with actual data)
-const dataPoints = [
-  {group: "15JSP09", x: 0.5, y: 0.5},
-  {group: "15SP02", x: 1.5, y: 1.5}
-];
-
 
 
 
