668d8be3455b746d934e9f4816389ee5332dee96
Svndda <aaroncz032@gmail.com>
2024-08-30 16:58:37 -0600

    feat: Temp and Date selector refactor.

--- Android/Development/app/src/main/java/com/inii/geoterra/development/fragments/FormFragment.kt
+++ Android/Development/app/src/main/java/com/inii/geoterra/development/fragments/FormFragment.kt
@@ -9,16 +9,20 @@ import android.media.ExifInterface
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.slider.Slider
 import com.inii.geoterra.development.R
 import com.inii.geoterra.development.components.OnFragmentInteractionListener
 import com.inii.geoterra.development.components.api.Error
@@ -53,55 +57,88 @@ class FormFragment : Fragment() {
     val locationButton = rootView.findViewById<Button>(R.id.userLocationButton)
     val imageButton = rootView.findViewById<Button>(R.id.locationImageButton)
     val sendButton = rootView.findViewById<Button>(R.id.sendRequestButton)
-    val dateEditText = rootView.findViewById<EditText>(R.id.dateTxtInput)
+    val dateText = rootView.findViewById<TextView>(R.id.dateText)
+    val temperatureSlider = rootView.findViewById<Slider>(R.id.temperatureSlider)
+    val sliderLabel = rootView.findViewById<TextView>(R.id.sliderLabel)
 
-    // Crear un calendario con la fecha actual
-    val calendar = Calendar.getInstance()
+    temperatureSlider.addOnChangeListener { _, value, _ ->
+      // Update the label text GRAVITY based on the selected value
+      val layoutParams = sliderLabel.layoutParams as LinearLayout.LayoutParams
+      layoutParams.gravity = when (value.toInt()) {
+        1 -> Gravity.START   // El Slider se alineará al inicio (izquierda)
+        2 -> Gravity.CENTER  // El Slider se centrará
+        3 -> Gravity.END     // El Slider se alineará al final (derecha)
+        else -> Gravity.START
+      }
+      sliderLabel.layoutParams = layoutParams
 
-    // Definir un formato de fecha
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-
-    // Configurar el DatePickerDialog
-    val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-      calendar.set(Calendar.YEAR, year)
-      calendar.set(Calendar.MONTH, month)
-      calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-      // Formatear la fecha seleccionada y mostrarla en el EditText
-      dateEditText.setText(dateFormat.format(calendar.time))
+      // Update the label text
+      val label = when (value.toInt()) {
+        1 -> "Frío"
+        2 -> "Tibio"
+        3 -> "Caliente"
+        else -> ""
+      }
+      sliderLabel.text = label
     }
 
+    // Create a calendar with the current date
+    val calendar = Calendar.getInstance()
+
+    // Define the date format
+    val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+
+    // Set a click listener for the location button
     locationButton.setOnClickListener {
       if (!GPSManager.isInitialized()) {
           GPSManager.initialize(requireContext())
       } else {
+        // Get the last known location.
         val userLocation = GPSManager.getLastKnownLocation()
         Log.i("Coordenadas", "Latitud ${userLocation?.latitude}" +
                 "y longitud ${userLocation?.longitude}")
         if (userLocation != null) {
           Toast.makeText(requireContext(), "Latitud ${userLocation.latitude} y longitud ${userLocation.longitude}", Toast.LENGTH_SHORT).show()
+          // Set the coordinates in the request form.
           requestForm.coordinates = "${userLocation.latitude}, ${userLocation.longitude}"
           Log.i("Coordenadas", requestForm.coordinates)
         }
       }
     }
 
+    // Set a click listener for the image button
     imageButton.setOnClickListener {
       Log.i("GalleryManager", "isInitialize: ${GalleryManager.isInitialize()}")
       if (!GalleryManager.isInitialize()) {
+        // Initialize the GalleryManager.
         GalleryManager.initialize(requireContext())
       } else {
+        // Open the gallery.
         openGallery()
       }
     }
 
-    dateEditText.setOnClickListener {
-      DatePickerDialog(
+    // Set a click listener for the date text
+    val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+      // Update the calendar with the selected date
+      calendar.set(Calendar.YEAR, year)
+      calendar.set(Calendar.MONTH, month)
+      calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+      // Set the date text with the selected date
+      dateText.text = dateFormat.format(calendar.time)
+    }
+
+    dateText.setOnClickListener {
+      // Show the date picker dialog
+      val datePickerDialog = DatePickerDialog(
         requireContext(),
+        android.R.style.Theme_Material_Light_Dialog,
         dateSetListener,
         calendar.get(Calendar.YEAR),
         calendar.get(Calendar.MONTH),
         calendar.get(Calendar.DAY_OF_MONTH)
-      ).show()
+      )
+      datePickerDialog.show()  // Show the dialog
     }
 
     sendButton.setOnClickListener {
@@ -116,9 +153,10 @@ class FormFragment : Fragment() {
   private fun getFormData() {
     lifecycleScope.launch {
       try {
+        // Accessing all the given data in the form.
         val name = rootView.findViewById<EditText>(R.id.nameTxtInput).text.toString()
         val phoneNumber = rootView.findViewById<EditText>(R.id.phoneNumberTxtInput).text.toString().toInt()
-        val date = rootView.findViewById<EditText>(R.id.dateTxtInput).text.toString()
+        val date = rootView.findViewById<EditText>(R.id.dateText).text.toString()
         val thermalSensation = rootView.findViewById<EditText>(R.id.temperatureSlider).text.toString().toInt()
         // TODO : CORREGIR EL DE LA TEMPERATURA PARA QUE COINCIDA CON EL SLIDER.
         val zoneOwner = rootView.findViewById<EditText>(R.id.zoneOwnerTxtInput).text.toString()
@@ -126,6 +164,7 @@ class FormFragment : Fragment() {
         val address = rootView.findViewById<EditText>(R.id.indicationsTxtInput).text.toString()
         val bubbles = if (rootView.findViewById<CheckBox>(R.id.bubbleCheckBox).isChecked) 1 else 0
 
+        // Set the data in the request form.
         requestForm.pointID = name
         requestForm.contactNumber = phoneNumber
         requestForm.date = date
@@ -144,14 +183,17 @@ class FormFragment : Fragment() {
   }
 
   private fun sendRequest() {
+    // Create a new request.
     val apiService = RetrofitClient.getAPIService()
     val call = apiService.newRequest(requestForm.pointID, requestForm.region, requestForm.date,
       requestForm.owner, requestForm.currentUsage, requestForm.address, requestForm.contactNumber,
       requestForm.coordinates ,requestForm.thermalSensation, requestForm.bubbles)
 
+    // Send the request.
     call.enqueue(object : Callback<RequestResponse>{
       override fun onResponse(call: Call<RequestResponse>, response: Response<RequestResponse>) {
         if (response.isSuccessful) {
+          // Handle the response.
           val serverResponse = response.body()
           if (serverResponse != null) {
             val status = serverResponse.status
@@ -178,18 +220,19 @@ class FormFragment : Fragment() {
   }
 
   private fun handleServerErrors(errors : List<Error>) {
-    // Verifica si la lista de errores no es nula y no está vacía
+    // Handle the server errors.
     for (error in errors) {
       Log.i(error.type, error.message)
     }
   }
 
   private fun showError(message: String) {
-    // Se le muestra el mensaje de error al usuario.
+    // Show an error message to the user.
     Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
   }
 
   private fun openGallery() {
+    // Open the gallery to select an image.
     lifecycleScope.launch {
       val intent = Intent(Intent.ACTION_PICK)
       intent.type = "image/*"
@@ -197,7 +240,9 @@ class FormFragment : Fragment() {
     }
   }
 
-  private val pickImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+  private val pickImageLauncher =
+    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+      // Handle the result of the image selection.
     if (result.resultCode == Activity.RESULT_OK) {
       val data: Intent? = result.data
       if (data != null && data.data != null) {
@@ -209,10 +254,8 @@ class FormFragment : Fragment() {
   }
 
   private fun handleImageUri(imageUri: Uri?) {
-    // Guarda el URI para uso posterior
-    // Puedes almacenarlo en una variable, base de datos, o enviarlo a otra parte de tu aplicación
+    // Manage the URI of the selected image.
     imageUri?.let {
-      // Ejemplo de almacenamiento en una variable de instancia
       extractLocationFromImage(it)
     }
   }
@@ -223,6 +266,7 @@ class FormFragment : Fragment() {
     try {
       val context = requireContext()
       inputStream = context.contentResolver.openInputStream(uri)
+      // Extract the location information from the EXIF data of the image.
       if (inputStream != null) {
         val exifInterface = ExifInterface(inputStream)
         val latLong = FloatArray(2)
@@ -233,6 +277,7 @@ class FormFragment : Fragment() {
           Log.i("Image Coor", "Latitud: $latitude, Longitud: $longitude")
           Toast.makeText(requireContext(), "Latitud: $latitude, Longitud: $longitude", Toast.LENGTH_SHORT).show()
           requestForm.coordinates = "$latitude, $longitude"
+          // You can use the latitude and longitude values as needed.
         } else {
           Toast.makeText(requireContext(), "La imagen seleccionada no contiene información de coordenadas.", Toast.LENGTH_SHORT).show()
         }
@@ -244,17 +289,7 @@ class FormFragment : Fragment() {
       Toast.makeText(requireContext(), "Error al leer los metadatos EXIF.", Toast.LENGTH_SHORT).show()
     } finally {
       inputStream?.close()
-    }
-  }
-
-  private fun isValidDate(date: String): Boolean {
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-    return try {
-      dateFormat.isLenient = false
-      dateFormat.parse(date)
-      true
-    } catch (e: Exception) {
-      false
+      // Handle the case where inputStream is null.
     }
   }
 
