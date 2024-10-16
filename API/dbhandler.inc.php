cd877763687cd1672f49915f6fb47c54734f1d0c
chris4fun <christopheracostam14@gmail.com>
2024-10-16 15:41:46 -0600

    FEAT/FIX: Agregado de archivo .config funcional

--- API/dbhandler.inc.php
+++ API/dbhandler.inc.php
@@ -1,6 +1,8 @@
 <?php
 
-$config = parse_ini_file('/path/to/config.ini', true);
+$configFilePath = __DIR__ . '/.config.ini';
+$config = parse_ini_file($configFilePath, true);
+
 $host = $config['database']['host'];
 $dbname = $config['database']['name'];
 $user = $config['database']['user'];
@@ -10,40 +12,10 @@ try {
     // Attempt to create a PDO connection using the extracted credentials
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo "Connection successful!";
     
 } catch (PDOException $e) {
     // Handle the error by displaying the error message (can be logged as well)
     echo "Connection failed: " . $e->getMessage();
 }
 
-if ($pdo) {
-    // Aquí puedes realizar operaciones con $pdo
-} else {
-    die("No se pudo establecer una conexión válida con la base de datos.");
-}
 
-// Hacemos accesible la variable $usedCred fuera de este archivo
-$GLOBALS['usedCred'] = $usedCred;
-
-// For anyone
-// $host = 'localhost';
-// $dbname = 'GeoterRA';
-// $user = 'root';
-// $pass = '';
-
-// For Mario :D
-// $host = 'localhost';
-// $dbname = 'GeoterRA';
-// $user = 'mario';
-// $pass = '2003';
-
-// try {
-//   $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $user, $pass);
-//   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//   die("Connection failed: " . $e->getMessage());
-// }
-
-?>
