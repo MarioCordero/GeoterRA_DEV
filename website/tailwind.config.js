f593edf01a000df7e11747572fd20a1c0dced0dc
chris4fun <christopheracostam14@gmail.com>
2024-11-09 03:14:07 -0600

    STYLE: Agregado de archivos necesarios para tailwind

---
+++ website/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./*.html",           // Adjust this path to match your project structure
+    "./*.php", 
+    "./assets/js/*.js",    // Add JavaScript file paths if Tailwind classes are used there
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
