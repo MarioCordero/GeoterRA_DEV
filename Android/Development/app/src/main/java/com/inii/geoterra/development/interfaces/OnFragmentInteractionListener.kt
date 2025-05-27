159a2ab5266c3b49d229a46b2c14ed45707245af
Aaron Sanchez <aaroncz032@gmail.com>
2025-05-26 19:11:03 -0600

    refactor: App page interface refactored.

--- Android/Development/app/src/main/java/com/inii/geoterra/development/interfaces/OnFragmentInteractionListener.kt
+++
@@ -1,8 +0,0 @@
-package com.inii.geoterra.development.interfaces
-
-interface OnFragmentInteractionListener {
-  fun onFragmentFinished() {
-  }
-  fun onFragmentFinished(pointLat : Double, pointLong : Double) {
-  }
-}
--- Android/Development/app/src/main/java/com/inii/geoterra/development/interfaces/PageFragment.kt
+++ Android/Development/app/src/main/java/com/inii/geoterra/development/interfaces/PageFragment.kt
@@ -1,18 +1,48 @@
 package com.inii.geoterra.development.interfaces
 
-import android.os.Bundle
-import android.view.View
-import androidx.activity.OnBackPressedCallback
+import android.content.Context
+import android.util.Log
 import androidx.fragment.app.Fragment
+import com.inii.geoterra.development.api.APIService
+import com.inii.geoterra.development.api.RetrofitClient
 
-open class PageFragment : Fragment(), OnFragmentInteractionListener {
-  open fun onHide() {
-     // Deletes all fragments in the childFragmentManager
-     childFragmentManager.fragments.forEach { fragment ->
-       childFragmentManager.beginTransaction().remove(fragment).commit()
-     }
+open class PageFragment : Fragment(), FragmentListener {
+  protected val apiService: APIService = RetrofitClient.getAPIService()
+  protected var listener: FragmentListener? = null
+
+  final override fun onAttach(context: Context) {
+    super.onAttach(context)
+
+    val parent = parentFragment
+    if (parent is FragmentListener) {
+      this.listener = parent
+      Log.i("PageFragment", "FragmentListener conectado con fragmento padre.")
+    } else if (context is FragmentListener) {
+      this.listener = context
+      Log.i("PageFragment", "FragmentListener conectado con activity.")
+    } else {
+      Log.w("PageFragment", "Ni el contexto ni el fragmento padre implementan FragmentListener.")
+    }
   }
-  open fun onShow() {
 
+  final override fun onDetach() {
+    super.onDetach()
+    this.listener = null
+  }
+
+  open fun onBackPressed(): Boolean {
+    // Por defecto no consume el evento
+    return false
+  }
+
+  open fun onHide() {
+    // Deletes all fragments in the childFragmentManager
+    childFragmentManager.fragments.forEach { childFragment ->
+      childFragmentManager.beginTransaction().remove(childFragment).commit()
+    }
+  }
+
+  open fun onShow() {
+    // Hook para cuando se muestra este fragmento
   }
 }
