9f186bc5544c4b7492b1bf87d6df55ddbc27f535
Aaron Sanchez <aaroncz032@gmail.com>
2025-06-19 11:38:02 -0600

    project settings updated

--- Android/Development/app/build.gradle.kts
+++ Android/Development/app/build.gradle.kts
@@ -1,9 +1,19 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.googleAndroidLibrariesMapsplatformSecretsGradlePlugin)
     alias(libs.plugins.kotlinParcelize)
 }
+
+val localProperties = Properties().apply {
+    val localFile = rootProject.file("local.properties")
+    if (localFile.exists()) {
+        load(localFile.inputStream())
+    }
+}
+
 
 android {
     namespace = "com.inii.geoterra.development"
@@ -19,16 +29,35 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    signingConfigs {
+        create("release") {
+            storeFile = file(localProperties["RELEASE_STORE_FILE"] as String)
+            storePassword = localProperties["RELEASE_STORE_PASSWORD"] as String
+            keyAlias = localProperties["RELEASE_KEY_ALIAS"] as String
+            keyPassword = localProperties["RELEASE_KEY_PASSWORD"] as String
         }
     }
 
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro")
+
+            buildConfigField("String", "API_BASE_URL",
+                             "\"${localProperties["API_BASE_URL"]}\"")
+        }
+
+        debug {
+            buildConfigField("String", "API_BASE_URL",
+                             "\"${localProperties["API_BASE_URL"]}\"")
+        }
+    }
 
 
     compileOptions {
@@ -49,7 +78,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.material.v190)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.livedata.ktx)
@@ -57,27 +86,40 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.play.services)
-    //noinspection UseTomlInstead
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    //noinspection UseTomlInstead
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    //noinspection UseTomlInstead
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    //noinspection UseTomlInstead
-    implementation("org.json:json:20240303")
-    implementation(libs.material.v1110)
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.squareup.converter.gson)
+    implementation(libs.okhttp.v4120)
+    implementation(libs.json)
     implementation(libs.play.services.location)
-    //noinspection UseTomlInstead
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    //noinspection UseTomlInstead
-    implementation("org.osmdroid:osmdroid-android:6.1.18")
-    implementation("androidx.exifinterface:exifinterface:1.3.7")
-    implementation("androidx.exifinterface:exifinterface:1.3.7")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("org.locationtech.proj4j:proj4j:1.1.0")
-    // implementation("org.jetbrains.kotlin:kotlin-android-extensions-runtime:<kotlin_version>")
+    implementation(libs.androidx.preference.ktx)
+    implementation(libs.osmdroid.android)
+    implementation(libs.androidx.exifinterface.v137)
+    implementation(libs.exifinterface)
+    implementation(libs.logging.interceptor)
+    implementation(libs.locationtech.proj4j)
     implementation(libs.firebase.firestore)
+    implementation(libs.androidx.junit.ktx)
+
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.mockwebserver)
+    testImplementation(libs.retrofit2.retrofit)
+    testImplementation(libs.squareup.converter.gson)
+
+    androidTestImplementation(libs.androidx.core)
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
+    androidTestImplementation(libs.androidx.fragment.testing.v157)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.mockwebserver)
+    androidTestImplementation(libs.mockito.core)
+    androidTestImplementation(libs.mockito.kotlin)
+    androidTestImplementation(libs.androidx.fragment.testing)
+    androidTestImplementation("androidx.fragment:fragment-testing:1.6.0")
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.byte.buddy)
+    testImplementation(libs.byte.buddy)
+    androidTestImplementation(libs.byte.buddy)
 }
