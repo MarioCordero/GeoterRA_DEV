70b152d2f6c302a40386096948a2a85fb0135698
Mario Cordero <mariogabriel.cordero@ucr.ac.cr>
2025-10-22 12:04:54 -0600

    FEAT: Eliminando Android de Main

--- Android/Development/app/src/main/java/com/inii/geoterra/development/interfaces/PermissionRequester.kt
+++
@@ -1,9 +0,0 @@
-package com.inii.geoterra.development.interfaces
-
-import android.content.Context
-
-interface PermissionRequester {
-  fun requestPermission(permissions: Array<String>, requestCode: Int)
-  fun shouldShowRationale(permission: String): Boolean
-  fun getContext(): Context
-}
