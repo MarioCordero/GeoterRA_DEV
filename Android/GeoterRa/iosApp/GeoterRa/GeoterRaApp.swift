9faddb1e5f245e8ab21f540f5df86f9f84de7bd9
Aaron Sanchez <111822002+Svndda@users.noreply.github.com>
2026-02-07 23:19:49 -0600

    config for mac ussage

---
+++ Android/GeoterRa/iosApp/GeoterRa/GeoterRaApp.swift
@@ -0,0 +1,18 @@
+//
+//  GeoterRaApp.swift
+//  GeoterRa
+//
+//  Created by Aaron Carmona Sanchez on 7/2/26.
+//  Copyright © 2026 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GeoterRaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Android/GeoterRa/iosApp/iosApp/Untitled.swift
@@ -0,0 +1,8 @@
+//
+//  Untitled.swift
+//  iosApp
+//
+//  Created by Aaron Carmona Sanchez on 7/2/26.
+//  Copyright © 2026 orgName. All rights reserved.
+//
+
