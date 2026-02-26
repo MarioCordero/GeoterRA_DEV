3be32cd1a595dafd5121cd4119cf98ffdf370553
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2026-02-26 16:43:18 -0600

    # REFRACT/FEAT - Adding my develop env

--- API/config/cors.php
+++ API/config/cors.php
@@ -5,20 +5,21 @@ declare(strict_types=1);
 $allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'http://geoterra.com',
+    'http://geoterra.com:5173',
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 if (in_array($origin, $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: $origin");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Origin: http://geoterra.com:5173');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Session-Token');
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
 }
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
+    http_response_code(200);
+    exit();
 }
