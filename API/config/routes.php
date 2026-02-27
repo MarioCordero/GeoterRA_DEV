68d1be23a11b7d0340bc2b5ffd6825c2187c6f25
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2026-02-26 22:01:54 -0600

    # REFRACT/FEAT - Changing of namespace the user register

--- API/config/routes.php
+++ API/config/routes.php
@@ -3,13 +3,13 @@
 return [
     // Auth routes
     ['method' => 'POST', 'path' => '/auth/refresh', 'controller' => 'AuthController', 'action' => 'refresh'],
-    ['method' => 'POST', 'path' => '/auth/register', 'controller' => 'AuthController', 'action' => 'register'],
     ['method' => 'POST', 'path' => '/auth/login', 'controller' => 'AuthController', 'action' => 'login'],
     ['method' => 'POST', 'path' => '/auth/logout', 'controller' => 'AuthController', 'action' => 'logout'],
     
     // User routes
     ['method' => 'GET', 'path' => '/users/me', 'controller' => 'UserController', 'action' => 'show'],
     ['method' => 'PUT', 'path' => '/users/me', 'controller' => 'UserController', 'action' => 'update'],
+    ['method' => 'POST', 'path' => '/users/register', 'controller' => 'UserController', 'action' => 'register'],
     ['method' => 'DELETE', 'path' => '/users/me', 'controller' => 'UserController', 'action' => 'delete'],
     
     // Analysis request routes
