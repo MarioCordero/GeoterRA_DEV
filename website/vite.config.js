91e8f4d4429ed5be743b3384436b61e8b885dd1e
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2026-04-21 22:59:54 -0600

    # REFACT:
    - Changing the localhost set

--- website/vite.config.js
+++ website/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
     host: 'geoterra.com',
     port: 5173,
+    allowedHosts: ['geoterra.com'],
     proxy: {
       '/API': {
         target: 'http://geoterra.com',
