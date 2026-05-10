80b4a0c2bf639b1b6dbe7a60fbab5aaa4ca6f1ad
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2026-05-10 11:59:33 -0600

    # REFACT:
    - Adjusting docusaurus

--- API/src/Core/OpenApiConfig.php
+++ API/src/Core/OpenApiConfig.php
@@ -1,22 +1,29 @@
 <?php
-// src/Core/OpenApiConfig.php
 namespace Core;
 
-use OpenApi\Attributes as OA;
-
-#[OA\Info(title: "GeoterRA API", version: "1.0.0", description: "API para la gestión de puntos geográficos")]
-#[OA\Server(url: "http://localhost:8000", description: "Servidor Local")]
-
-// Definición de Seguridad para el generador de Postman
-#[OA\SecurityScheme(
-    securityScheme: "cookieAuth",
-    type: "apiKey",
-    in: "cookie",
-    name: "session_token"
-)]
-#[OA\SecurityScheme(
-    securityScheme: "tokenAuth",
-    type: "http",
-    scheme: "bearer"
-)]
+/**
+ * @OA\Info(
+ * title="GeoterRA API",
+ * version="1.0.0",
+ * description="API para la gestión de puntos geográficos y manifestación de puntos"
+ * )
+ * @OA\Server(
+ * url="http://localhost:8000",
+ * description="Servidor de Desarrollo Local"
+ * )
+ * * @OA\SecurityScheme(
+ * securityScheme="cookieAuth",
+ * type="apiKey",
+ * in="cookie",
+ * name="session_token",
+ * description="Sesión para clientes Web vía HttpOnly Cookie"
+ * )
+ * * @OA\SecurityScheme(
+ * securityScheme="tokenAuth",
+ * type="http",
+ * scheme="bearer",
+ * bearerFormat="JWT",
+ * description="Autenticación para App Android vía Token"
+ * )
+ */
 class OpenApiConfig {}
